0b53e1656bbbd631b06573b6b612e6cc6447dd71
Shauryacious <shauryaagrawal2718@gmail.com>
2024-01-15 18:34:36 +0530

    Learned Linked list

--- Linked List/basic#1.cpp
+++ Linked List/basic#1.cpp
@@ -4,20 +4,39 @@ class Node{
 public:
   int val;
   Node* next;
-  Node(int val){
+  Node(int val){ //constructor fn
     this->val = val;
     this->next = NULL;
   }
-
-
-  void display(Node* head){
-    Node* temp = head;
-    while(temp->next != NULL){
-      cout<<temp->val<<" ";
-      temp = temp->next;
-    }
-  }
 };
+
+
+//DISPLAY LL NORMAL ITERATIVE SOLUTION -> Better soln
+void display(Node* head){
+  Node* temp = head;
+  while(1){
+    cout<<temp->val<<" ";
+    if(temp->next == NULL) break;
+    temp = temp->next;
+  }
+}
+
+//DISPLAY LL RECURSIVE SOLUTION -> Bad soln because every time new dabba is created 
+void displayRec(Node* head){
+  cout<<head->val<<" ";
+  if(head->next == NULL) return;
+  displayRec(head->next);
+}
+
+//
+int sizeOfLL(Node* head){
+  int n = 0;
+  while(head != NULL){
+    n++;
+    head = head->next;
+  }
+  return n;
+}
 int main(){
   Node* a = new Node(10);
   Node* b = new Node(20);
@@ -28,6 +47,15 @@ int main(){
   b->next = c;
   c->next = d;
 
+
+  // cout<<a->next<<endl;
+  // cout<<b;
+
+
   Node* head = a;
-  Node.display(head);
+  //display(head);
+
+  displayRec(head);
+  cout<<endl;
+  //cout<<sizeOfLL(head); //yaha a pointer(head) pass by value hua hai -> so recursive solution will be bad because time complexity will remain same but space complexity will increase
 }
---
+++ Linked List/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    if(temp->next == NULL) break;
