e9e7b8e87a17658051c9bfedd0c43aa3e84a9f1a
Shauryacious <shauryaagrawal2718@gmail.com>
2024-07-08 10:05:10 +0530

    DSU

--- Graph/06-DSU-DisjointSetUnion-Find_Uninon/03-Detect-Cycle-with-DSU/code.cpp
+++ Graph/06-DSU-DisjointSetUnion-Find_Uninon/03-Detect-Cycle-with-DSU/code.cpp
@@ -1,30 +1,30 @@
 class Solution{
-vector<int> PARENT;
-vector<int> rank;
+    vector<int> PARENT;
+    vector<int> rank;
 
-int find(int x){ //find the parent of x
-    if(PARENT[x] == x) return x;
-    return PARENT[x] = find(PARENT[x]);
-}
+    int find(int x){ 
+        if(PARENT[x] == x) return x;
+        return PARENT[x] = find(PARENT[x]);
+    }
 
-void Union(int x, int y){
-    int parent_x = find(x);
-    int parent_y = find(y);
+    void Union(int x, int y){
+        int parent_x = find(x);
+        int parent_y = find(y);
 
-    if(parent_x == parent_y) return; //already in the same set
-    else{ // not in the same set, hence union them
-        if(rank[parent_x] > rank[parent_y]){ //Jiski rank jada hai wo papa banega
-            PARENT[parent_y] = parent_x; //toh x papa banega y ka
-        }
-        else if(rank[parent_y] > rank[parent_x]){
-            PARENT[parent_x] = parent_y; //toh y papa banega x ka
-        }
-        else{ //rank of both are same
-            PARENT[parent_x] = parent_y; //toh y papa banega x ka
-            rank[parent_y]++; //because y is a parent now so, rank of y is increased by 1
+        if(parent_x == parent_y) return; 
+        else{
+            if(rank[parent_x] > rank[parent_y]){ 
+                PARENT[parent_y] = parent_x;
+            }
+            else if(rank[parent_y] > rank[parent_x]){
+                PARENT[parent_x] = parent_y; 
+            }
+            else{ 
+                PARENT[parent_x] = parent_y; 
+                rank[parent_y]++; 
+            }
         }
     }
-}
 public:
     //Function to detect cycle using DSU in an undirected graph.
 	int detectCycle(int V, vector<int>adj[]){
